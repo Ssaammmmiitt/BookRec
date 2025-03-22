ac714c0394c75f52db311e43e534a5f657cc5d9a
Sammit Poudyal <poudyal.sammit@gmail.com>
2025-03-22 20:23:42 +0545

    enhance user model and authentication routes with additional fields and error handling

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,23 +5,22 @@ import jwt from "jsonwebtoken";
 const router = express.Router();
 
 const generateToken = (u_Id) => {
-    return jwt.sign({u_Id},process.env.JWT_SECRET,{expiresIn:"30d"});
-}
-
-router.post("/login", async (req, res) => {
-  res.send("login route");
-});
+  return jwt.sign({ u_Id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 router.post("/register", async (req, res) => {
   try {
-    const { email, username, password, gender } = req.body;
-    if (!username || !email || !password || !gender) {
+    let { email, username, password, gender } = req.body;
+    if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
     if (password.length < 8) {
       return res
         .status(400)
         .json({ message: "Password must be at least 8 characters" });
+    }
+    if (!gender) {
+      gender = "Male";
     }
     if (username.length < 3) {
       return res
@@ -42,32 +41,66 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ message: "Username already taken!" });
     }
 
-      const profilePic = (gender="Male") ? `https://avatar.iran.liara.run/public/boy?username=${username}`:`https://avatar.iran.liara.run/public/girl?username=${username}`;
-    
+    const profilePic =
+      gender === "Male"
+        ? `https://avatar.iran.liara.run/public/boy?username=${username}`
+        : `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const user = new User({
       username,
       email,
       password,
       gender,
-      profilePic
+      profilePic,
     });
 
     await user.save();
 
     const token = generateToken(user._id);
 
-    res.status(201).json({ token, user:{
-        _id:user._id,
-        username:user.username,
-        email:user.email,
-        profilePic:user.profilePic,
-    }})
-
-} catch (error) {
+    res.status(201).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profilePic: user.profilePic,
+      },
+    });
+  } catch (error) {
     console.log("Error in register route !!!", error);
-    res.status(500).json({message:"Internal Server Error"});
-}
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    //checking if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    //check if passwords match
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user._id);
+    res.status(200).json({
+        token,
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email,
+            profilePic:user.profilePic,
+        }
+    })
+  } catch (error) {
+    console.log("Error in login route !!!", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 export default router;
